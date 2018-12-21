939e16c8f2cebe7b632690108ef890b0b27530a2
Archonsh <archonshen@gmail.com>
2018-12-21 13:15:05 +0800

    add cuda setup

---
+++ setup_cuda.sh
@@ -0,0 +1,3 @@
+export CUDA_HOME=/usr/local/cuda-9.0
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64
+export PATH=$PATH:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/usr/local/cuda-9.0/bin:/snap/bin
